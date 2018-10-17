d96a4a7a8976933b5c43fed400dc0baa9a4088f5
tongtong <1053673440@qq.com>
2018-10-17 21:14:15 +0800

    添加了按钮的loading状态 添加了按钮组

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,9 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-    <svg v-if="icon" class="icon">
-      <use :xlink:href="`#i-${icon}`"></use>
-    </svg>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
+    <g-icon class='icon' v-if="icon && !loading" :name='icon'></g-icon> 
+    <g-icon class='loading icon' v-if="loading"  name='spin'></g-icon>
     <div class="content">
-      <slot><slot>
+      <slot/>
     </div>
   </button>
 </template>
@@ -13,11 +12,14 @@
   export default {
     props: {
       icon: {},
+      loading: {
+        type: Boolean,
+        default: false,
+      },
       iconPosition: {
         type: String,
         default: 'left',
         validator(value) {
-          console.log(value)
           if(value !== 'left' && value !== 'right') {
             return false
           } else {
@@ -29,6 +31,10 @@
   }
  </script>
  <style lang='scss'>
+  @keyframes spin {
+    0% {transform: rotate(0deg);}
+    100% {transform: rotate(360deg);}
+  }
      .g-button {
       font-size: var(--font-size);
       height: var(--button-height);
@@ -37,6 +43,8 @@
       border-radius: var(--border-radius);
       background: var(--button-bg);
       display: inline-flex;
+      justify-content: center;
+      align-items: center;
       //内联元素不对齐解决方法
       vertical-align: middle;
       &:hover {
@@ -58,6 +66,9 @@
       &.icon-right {
         > .icon {order: 2; margin-right: 0; margin-left: .2em} 
         > .content {order: 1;}
+      }
+      .loading {
+        animation: spin 2s infinite linear;
       }
     }
   
