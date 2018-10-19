a4ddc070c931f7d3a6f80740b151934c2b73240b
tongtong <1053673440@qq.com>
2018-10-19 17:35:28 +0800

    使用chai进行单元测试 对按钮的功能进行了测试

--- src/app.js
+++ src/app.js
@@ -14,3 +14,87 @@ new Vue({
     }
   }
 })
+
+//单元测试   输入和输出是否是期待的
+
+import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
+const expect = chai.expect
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  let vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount(div)
+  let useElement = vm.$el.querySelector('use')
+
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+//测试spin图标
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  let vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  vm.$mount(div)
+  let useElement = vm.$el.querySelector('use')
+
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-spin')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+//测试文字和图标的order
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  let vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let order = window.getComputedStyle(svg, null).order
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+//测试button的click事件   使用chai-spy
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  let button = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  button.$mount(div)
+  //期望这个函数被调用
+
+  const spy = chai.spy(function() {console.log(1)})
+  button.$on('click', spy)
+  button.$el.click()
+  expect(spy).to.have.been.called()
+  button.$el.remove()
+  button.$destroy()
+}
