6819518d06a90b24bf255e4dcfe1a8dff031ed3a
tongtong <1053673440@qq.com>
2019-01-13 09:57:47 +0800

    '完成row和col雏形'

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import Row from './row'
+import Col from './col'
 
+//注册组件
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 new Vue({
   el: '#app',
   data() {
---
+++ src/row.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "GuLuRow",
+  props: {
+    gutter: {
+      type: [String, Number]
+    }
+  },
+  created() {
+    console.log('gutter:', this.gutter)
+    console.log("row created");
+  },
+  mounted() {
+    console.log("row mounted");
+    console.log(this.$children);
+    //吧gutter传递给孩子
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter;
+    });
+  }
+};
+</script>
+
+<style scope lang="scss">
+.row {
+  display: flex;
+}
+</style>
